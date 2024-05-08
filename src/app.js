da7c00f94c31872c6cfec876381b0df4e3a6d61d
Giorgio <155197460+giorgioabo@users.noreply.github.com>
2024-05-08 18:52:38 +0000

    generador de dominios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,29 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("dominios").innerHTML = generateDominios();
+  console.log("GND");
+};
+
+let generateDominios = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".net", ".com", ".es", ".us", ".io"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let domIndex = Math.floor(Math.random() * dom.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    adj[adjIndex] +
+    "" +
+    noun[nounIndex] +
+    "" +
+    dom[domIndex] +
+    ""
+  );
 };
