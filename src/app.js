af5a96e0c2a8249e2d2dd97cea68606810f59faf
Giorgio <155197460+giorgioabo@users.noreply.github.com>
2024-05-08 19:55:11 +0000

    GND

--- src/app.js
+++ src/app.js
@@ -5,29 +5,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  document.getElementById("dominios").innerHTML = generateDominios();
-  console.log("GND");
-};
-
-let generateDominios = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
-  let dom = [".net", ".com", ".es", ".us", ".io"];
+  let dom = [".com", ".net", ".us", ".io"];
 
-  let pronounIndex = Math.floor(Math.random() * pronoun.length);
-  let adjIndex = Math.floor(Math.random() * adj.length);
-  let nounIndex = Math.floor(Math.random() * noun.length);
-  let domIndex = Math.floor(Math.random() * dom.length);
-
-  return (
-    pronoun[pronounIndex] +
-    "" +
-    adj[adjIndex] +
-    "" +
-    noun[nounIndex] +
-    "" +
-    dom[domIndex] +
-    ""
-  );
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let c = 0; c < dom.length; c++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + dom[c]);
+        }
+      }
+    }
+  }
 };
